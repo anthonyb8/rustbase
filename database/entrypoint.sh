5ba4d2cd66886624ff1e4e6fe832343c2534dc68
anthonyb8 <anthonybaxter819@gmail.com>
2025-11-08 16:39:41 -0500

    Update entrypoint for database.

---
+++ database/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+if [ -z "$POSTGRES_URL" ]; then
+  echo "ERROR: POSTGRES_URL is not set!" >&2
+  exit 1
+fi
+echo "Using URL: ${POSTGRES_URL:0:20}..." >&2
+exec migrate apply --url "$POSTGRES_URL" --dir file:///migrations
--- scripts/deploy-atlas.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-exec migrate apply --url "$POSTGRES_URL" --dir file:///migrations
