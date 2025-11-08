ca34acc6d5d9489d668db8a9ad8557111401e915
anthonyb8 <anthonybaxter819@gmail.com>
2025-11-08 08:39:54 -0500

    Added script to deploy docker compose utilizing AWS SSM parameters.

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ENV_FILE=".env"
+
+aws ssm get-parameters-by-path --path "/testing/prod" --with-decryption \
+  --query "Parameters[*].[Name,Value]" --output text | while read name value; do
+  key=$(echo "$name" | awk -F'/' '{print toupper($NF)}')
+  echo "$key=$value"
+done >"$ENV_FILE"
+
+if ! docker compose up --build; then
+  rm -rf "$ENV_FILE"
+  exit 1
+fi
+
+rm -rf "$ENV_FILE"
+
+# docker build -t backend ./backend
+# docker tag backend:latest 186091750176.dkr.ecr.us-east-1.amazonaws.com/backend:latest
+# docker push 186091750176.dkr.ecr.us-east-1.amazonaws.com/backend:latest
